0729d861c7de4555e68e5b6c0921ea5e65609ea8
kun3741 <vaysed.dev@gmail.com>
2026-06-11 21:09:51 +0300

    chore: add ecosystem.config.js for PM2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'web-lady-api',
+      script: 'dist/apps/api/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'web-lady-workers',
+      script: 'dist/apps/workers/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
